a23396466cca33f6d615fe2941564abca4308093
Stefan.Iv PC <stefan.ivanus@outlook.de>
2023-09-14 20:58:10 +0200

    basic visual template

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 export default function Home() {
   return (
     <div className="grid grid-cols-3 grid-rows-6 h-screen">
-      <div className="col-span-3 bg-red-600 ">1</div>
-      <div className="col-span-3 row-span-3 row-start-2 bg-lime-700">2</div>
-      <div className="row-span-2 row-start-5 bg-amber-400">6</div>
-      <div className="col-span-2 row-span-2 row-start-5 bg-emerald-600">7</div>
+      <div className="col-span-3 h-20 bg-slate-600">1</div>
+      <div className="col-span-3 row-span-3 col-start-1 row-start-4 bg-red-600">
+        2
+      </div>
+      <div className="row-span-2 col-start-1 row-start-2 bg-lime-700">6</div>
+      <div className="col-span-2 row-span-2 col-start-2 row-start-2 bg-blue-400">
+        7
+      </div>
     </div>
   );
 }
