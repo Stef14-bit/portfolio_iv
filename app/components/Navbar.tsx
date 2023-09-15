d886fefb75e2300158e8e7d97f5bb847c254d882
Stefan.Iv PC <stefan.ivanus@outlook.de>
2023-09-15 21:50:56 +0200

    basic navbar visual

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  return (
+    <div className="bg-violet-600 w-full fixed flex items-center p-5">
+      <ul className="w-full flex flex-row justify-around items-center">
+        <li>title</li>
+        <li>resume</li>
+        <li>contact</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 export default function Home() {
   return (
-    <div className="grid grid-cols-3 grid-rows-6 h-screen">
-      <div className="col-span-3 h-20 bg-slate-600">1</div>
-      <div className="col-span-3 row-span-3 col-start-1 row-start-4 bg-red-600">
-        2
-      </div>
-      <div className="row-span-2 col-start-1 row-start-2 bg-lime-700">6</div>
-      <div className="col-span-2 row-span-2 col-start-2 row-start-2 bg-blue-400">
-        7
+    <div className="h-screen">
+      <div className="grid grid-cols-3 grid-rows-6 h-full">
+        <div className="col-span-3 bg-slate-600">1</div>
+        <div className="col-span-3 row-span-3 col-start-1 row-start-4 bg-red-600">
+          2
+        </div>
+        <div className="row-span-2 col-start-1 row-start-2 bg-lime-700">6</div>
+        <div className="col-span-2 row-span-2 col-start-2 row-start-2 bg-blue-400">
+          7
+        </div>
       </div>
     </div>
   );
