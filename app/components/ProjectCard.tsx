9e04fd533f3950635aeac24a549f8cfdc09fbf77
stefan <stefan@stefan.com>
2023-09-20 17:39:27 +0200

    improved projects card style

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -9,18 +9,18 @@ type Props = {
 
 const ProjectCard = (props: Props) => {
   return (
-    <div className="flex flex-col justify-center content-around border-2 rounded-md border-slate-400 m-5 p-5 ">
+    <div className="flex flex-col justify-center content-around border-2 rounded-md border-slate-400 m-5 p-5 hover:shadow-black hover:shadow-2xl">
       <iframe src={props.projectUrl} title={props.projectName}></iframe>
       <h2>{props.projectName}</h2>
       <p>{props.projectDescription}</p>
       <div className=" my-5 h-10 flex  items-center justify-around rounded-md font-light text-xs md:text-lg">
         <a
-          className="bg-green-500 h-full w-full flex items-center justify-center rounded-l-md hover:opacity-50 hover:shadow-lg hover:shadow-black "
+          className="bg-green-500 h-full w-full flex items-center justify-center rounded-l-md  hover:shadow-md hover:shadow-black"
           href={props.projectUrl}>
           Visit website
         </a>
         <a
-          className="bg-blue-500 h-full w-full flex items-center justify-center rounded-r-md"
+          className="bg-blue-500 h-full w-full flex items-center justify-center rounded-r-md hover:shadow-md hover:shadow-black"
           href={props.gitUrl}>
           Visit GitHub
         </a>
