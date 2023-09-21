59b12a3e3308730d8b66092c78679b1d102da18b
stefan <stefan@stefan.com>
2023-09-21 19:57:25 +0200

    navbar complete and back to top button started

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,8 +6,10 @@ const Navbar = (props: Props) => {
   return (
     <div className="bg-blue-300 w-full  flex items-center p-5">
       <ul className="w-full flex flex-row justify-around items-center">
-        <a href="#skills"></a>
-        <li>Skills</li>
+        <a href="#skills">
+          <li>Skills</li>
+        </a>
+
         <a href="#projects">
           <li>Projects</li>
         </a>
