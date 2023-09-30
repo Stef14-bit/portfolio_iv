0c69ee5184c45b3190ddc27e4572b60c127a524d
stefan <stefan@stefan.com>
2023-09-30 21:31:42 +0200

    added separate file for navlinks

---
+++ data/navLinks.ts
@@ -0,0 +1,10 @@
+type Links = { id: number; href: string };
+
+const navLinks: Links[] = [
+  { id: 1, href: "/" },
+  { id: 2, href: "#skills" },
+  { id: 3, href: "#projects" },
+  { id: 4, href: "../contact" },
+];
+
+export default navLinks;
