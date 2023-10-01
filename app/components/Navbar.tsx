ae012d277c403cc0e3dc097f098df4ed4fcb7642
stefan <stefan@stefan.com>
2023-10-01 20:58:22 +0200

    replace static links in navbar with a map

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import navLinks from "@/data/navLinks";
 
 type Props = {};
 
@@ -6,17 +7,11 @@ const Navbar = (props: Props) => {
   return (
     <div className="bg-blue-300 w-full  flex items-center p-5">
       <ul className="w-full flex flex-row justify-around items-center">
-        <a href="/">
-          <li>Home</li>
-        </a>
-        <a href="#skills">
-          <li>Skills</li>
-        </a>
-
-        <a href="#projects">
-          <li>Projects</li>
-        </a>
-        <a href="../contact">Contact me</a>
+        {navLinks.map((e) => (
+          <a key={e.id} href={e.href}>
+            <li>{e.name}</li>
+          </a>
+        ))}
       </ul>
     </div>
   );
